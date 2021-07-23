124f7bbf71e2e0ceeec476917431d996c0588ea0
Chirag Jain <chiragj1812@gmail.com>
2021-07-23 22:33:55 +0530

    endpoints declared  with their views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from api import views
 urlpatterns = [
-    path('', views.usersApi),
+    path('face_recognition/',views.face_recognition),
+    path('object_detection/',views.object_detection),
+    path('depth_perception/',views.depth_perception),
+    path('speech_to_text/',views.speech_to_text),
+    path('text_detection/',views.text_detection),
+    path('text_to_speech/',views.text_to_speech),
+    path('voice_command/',views.voice_command),
+    path('', views.testRoute),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here
-def usersApi(request):
+
+def face_recognition(request):
+    return HttpResponse("Ok face_recognition! Working ")
+
+def object_detection(request):
+    return HttpResponse("Ok object_detection! Working ")
+
+def depth_perception(request):
+    return HttpResponse("Ok depth_perception! Working ")
+
+def speech_to_text(request):
+    return HttpResponse("Ok speech_to_text! Working ")
+
+def text_detection(request):
+    return HttpResponse("Ok text_detection! Working ")
+
+def text_to_speech(request):
+    return HttpResponse("Ok text_to_speech! Working ")
+
+def voice_command(request):
+    return HttpResponse("Ok voice_command! Working ")
+
+def testRoute(request):
     return HttpResponse("Ok! Working ")
 
