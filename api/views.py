01e745df8042f9c88c993912842142b7dd7f7ab5
Chirag Jain <chiragj1812@gmail.com>
2021-07-23 13:32:16 +0530

    added api/ endpoint for test

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here
+def usersApi(request):
+    return HttpResponse("Ok! Working ")
 
-# Create your views here.
